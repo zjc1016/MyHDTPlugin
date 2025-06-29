f0f4c830e76992de10e9ef1fa8a378c3aff35307
zjc1016 <1455341164@qq.com>
2025-06-29 12:12:21 +0800

    更新

--- MyHDTPlugin/PetAction.cs
+++ MyHDTPlugin/PetAction.cs
@@ -2,12 +2,16 @@
 using Hearthstone_Deck_Tracker;
 using Hearthstone_Deck_Tracker.Controls;
 using Hearthstone_Deck_Tracker.Enums;
+using Hearthstone_Deck_Tracker.Enums.Hearthstone;
 using Hearthstone_Deck_Tracker.Hearthstone;
 using Hearthstone_Deck_Tracker.Hearthstone.RelatedCardsSystem;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,8 +26,69 @@ namespace MyHDTPlugin
     {
         private static MyPet Pet = null;
         private static ActivePlayer turn;
+        public class CardMatchRule
+        {
+            public string match { get; set; }
+            public string type { get; set; }  // "完全相同", "名称包含"
+            public string who { get; set; } //谁打出的
+            public string video { get; set; } //视频名称
+
+        }
+        private List<CardMatchRule> cardMatchRules;
+
+        public void LoadCardMatchRules(string path)
+        {
+            var json = File.ReadAllText(path);
+            cardMatchRules = JsonConvert.DeserializeObject<List<CardMatchRule>>(json);
+        }
+
+        public void CheckRule(Card card, ActivePlayer turn)
+        {
+            string cardName = GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN);
+
+            foreach (var rule in cardMatchRules)
+            {
+                // 判断适用人群
+                if (!((rule.who == "对方" && turn == ActivePlayer.Opponent) || (rule.who == "自己" && turn == ActivePlayer.Player) || rule.who == "双方"))
+                    continue;
+
+                // 判断匹配方式
+                switch (rule.type)
+                {
+                    case "完全相同":
+                        if (cardName == rule.match)
+                        {
+                            Pet.Update(rule.video, rule.video);
+                            return;
+                        }
+                        break;
+
+                    case "名称包含":
+                        if (cardName.Contains(rule.match))
+                        {
+                            Pet.Update(rule.video, rule.video);
+                            return;
+                        }
+                        break;
+
+                    case "特殊":
+                        if (rule.match == "#highHealthLowAttack" && card.Health >= 15 && card.Attack <= 5)
+                        {
+                            Pet.Update(rule.video, "超大型屁股");
+                            return;
+                        }
+                        break;
+                }
+            }
+
+            // 默认提示
+            Pet.Update("你使用卡牌：\n" + cardName, null);
+
+        }
+
         public PetAction(MyPet pet)
         {
+
             Pet = pet;
             // Hide in menu, if necessary
             if (Config.Instance.HideInMenu && CoreAPI.Game.IsInMenu)
@@ -38,6 +103,10 @@ namespace MyHDTPlugin
         }
         internal void TurnStart(ActivePlayer player)
         {
+            string dllDir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            string path = Path.Combine(dllDir, "rules.json");
+            LoadCardMatchRules(path);
+
             turn = player;
             Pet.Update(player + "回合开始！", null);
             var opp_entity = CoreAPI.Game.Opponent.PlayerEntities.ToList();
@@ -65,47 +134,52 @@ namespace MyHDTPlugin
         internal void GameStart()
         {
             Pet.Update("游戏开始！", "main");
+            string dllDir = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+            string Path = System.IO.Path.Combine(dllDir, "rules.jpg");
+            LoadCardMatchRules(Path);
         }
 
         public void OnPlayerPlay(Card card)
         {
-
-            if (GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN) == "亵渎")
-            {
-                Pet.Update("教科书般的亵渎！", "教科书般的亵渎");
-            }
-            else if (GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN) == "骨鸡蛋") {
-                Pet.Update("亡灵小鸡是亡灵吗？", "亡灵小鸡是亡灵吗");
-            }
-            else if (card.Health >= 15 && card.Attack <= 5)
+            CheckRule(card, turn);
+            //if (GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN) == "亵渎")
+            //{
+            //    Pet.Update("教科书般的亵渎！", "教科书般的亵渎");
+            //}
+            //else if (GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN) == "骨鸡蛋") {
+            //    Pet.Update("亡灵小鸡是亡灵吗？", "亡灵小鸡是亡灵吗");
+            //}
+            if (card.Health >= 15 && card.Attack <= 5)
             {
                 Pet.Update("你打出了一个超大型屁股！", "超大型屁股");
             }
-            else if (GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN).Contains("铜须"))
-            {
-                Pet.Update("铜须是生物吗？", "铜须是生物吗");
-            }
-            else
-            {
-                Pet.Update("你使用卡牌：\n" + GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN), null);
-            }
+            //else if (GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN).Contains("铜须"))
+            //{
+            //    Pet.Update("铜须是生物吗？", "铜须是生物吗");
+            //}
+            //else
+            //{
+            //    Pet.Update("你使用卡牌：\n" + GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN), null);
+            //}
         }
 
 
         public void OnOpponentPlay(Card card)
         {
-            if (GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN) == "骨鸡蛋")
-            {
-                Pet.Update("亡灵小鸡是亡灵吗？", "亡灵小鸡是亡灵吗");
-            }
-            else if (GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN).Contains("铜须"))
-            {
-                Pet.Update("铜须是生物吗？", "铜须是生物吗");
-            }
-            else
-            {
-                Pet.Update("对手使用卡牌：\n" + GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN), null);
-            }
+            CheckRule(card, turn);
+
+            //if (GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN) == "骨鸡蛋")
+            //{
+            //    Pet.Update("亡灵小鸡是亡灵吗？", "亡灵小鸡是亡灵吗");
+            //}
+            //else if (GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN).Contains("铜须"))
+            //{
+            //    Pet.Update("铜须是生物吗？", "铜须是生物吗");
+            //}
+            //else
+            //{
+            //    Pet.Update("对手使用卡牌：\n" + GetCardByDbfId(card.DbfId).GetLocName(HearthDb.Enums.Locale.zhCN), null);
+            //}
                 
         }
 
